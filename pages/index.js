613023032d4ee371402ddb021ac79aa31828cc7d
JesperSvensson00 <jesper.svensson00@hotmail.com>
2023-04-25 11:02:41 +0200

    Init

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Morgonmysboisen</h1>
+      <h2>Här hittar du våra senaste klipp!</h2>
+      <div>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/cP_h48Db69o"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen></iframe>
+      </div>
+    </main>
+  );
+}
+
