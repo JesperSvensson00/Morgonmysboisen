22d8fe07988a3e332d27752bd9da601331bf4154
Jespersvensson00 <jesper.svensson00@hotmail.com>
2023-04-29 02:31:53 +0200

    Lmm3 sida

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,25 @@
 import Head from "next/head";
+import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "@/styles/globals.css";
+import "@/styles/lmm.css";
 
+const inter = Inter({ subsets: ["latin"] });
 export default function App({ Component, pageProps }) {
   return (
-    <div>
+    <>
       <Head>
         <title>Morgonmysboisen</title>
         <meta name="description" content="Morgonmysboisens officiella webbsida" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <style jsx global>{`
+          html {
+            font-family: ${inter.style.fontFamily};
+          }
+        `}</style>
       </Head>
       <Component {...pageProps} />
-    </div>
+    </>
   );
 }
---
+++ pages/lmm3.js
@@ -0,0 +1,66 @@
+import Image from "next/image";
+
+export default function Lmm3({ images }) {
+  return (
+    <main id="lmm3">
+      <div className="content">
+        <div className="gallery left">
+          {images.map((image_path, idx) => {
+            if (idx % 2 === 0) {
+              return (
+                <div className="image-container" key={idx}>
+                  <Image
+                    src={`/images/lmm3/${image_path}`}
+                    alt="Kapitelbild"
+                    width={400}
+                    height={400}
+                  />
+                </div>
+              );
+            }
+          })}
+        </div>
+        <div>
+          <h1>Lir med morgonmysboisen III</h1>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/cP_h48Db69o"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+        </div>
+        <div className="gallery right">
+          {images.map((image_path, idx) => {
+            if (idx % 2 === 1) {
+              return (
+                <div className="image-container" key={idx}>
+                  <Image
+                    src={`/images/lmm3/${image_path}`}
+                    alt="Kapitelbild"
+                    width={400}
+                    height={400}
+                  />
+                </div>
+              );
+            }
+          })}
+        </div>
+      </div>
+    </main>
+  );
+}
+
+import { readdirSync } from "fs";
+import { join } from "path";
+export async function getStaticProps() {
+  const PATH = join(process.cwd(), "public/images/lmm3");
+  let images = readdirSync(PATH).map((path) => {
+    return path;
+  });
+  images = images.sort();
+  return {
+    props: {
+      images,
+    },
+  };
+}
