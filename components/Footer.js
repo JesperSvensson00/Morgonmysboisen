341b936db212d278d9f77dc54a27f4c129844d1f
JesperSvensson00 <jesper.svensson00@hotmail.com>
2023-06-20 05:52:05 +0200

    Gjorde en footer komponent

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div>
+        <p>Skapad av Mr Potato</p>
+      </div>
+    </footer>
+  );
+}
--- pages/start.js
+++ pages/start.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   const timeline_content = [
@@ -83,54 +84,60 @@ export default function Home() {
   };
 
   return (
-    <main id="start">
-      <div className="content">
-        <div id="header">
-          <h1>Morgonmysboisen</h1>
-          <Image src={"/images/grupp_bild.png"} width={2592} height={1434} alt="Bakgrund" />
-        </div>
-        <div className="link-list">
-          <Link href={"/lmm1"}>LMM1</Link>
-          <Link href={"/lmm2"}>LMM2</Link>
-          <Link href={"/lmm3"}>LMM3</Link>
-          <Link href={"/"}>Kokosboll</Link>
-          <Link href={"/"}>Art gallery</Link>
-          <Link href={"/"}>Behind the scenes</Link>
-        </div>
-        <div id="about">
-          <h2>Om Morgonmysboisen</h2>
-          <p>
-            Morgonmysboisen är en organisation vars syfte är att kunna erbjuda individer en plats
-            för roliga stunder och självklart mys.
-          </p>
-          <p className="review">
-            <q>Med Morgonmysboisen har jag kunnat integreras in i det svenska samhället.</q>
-            <br />- Benjamin Boros - gamer och kelgris.
-          </p>
-          <p>
-            Morgonmysboisen, tidigare kallad och även välkänd som Kvällsmysboisen, blev under 2022
-            ett officiellt kulturarv i Sverige efter organisationens framgångsrika arbete med humor
-            och glamor. Framgångarna såsom hit-triliogin &quot;Lir Med Morgonmysboisen&quot; och den
-            ökända låten &quot;Kokosboll&quot; satte organisationen på kartan och gjorde Ljungby
-            kommun till en plats för unga att utvecklas.
-          </p>
-          <p className="review">
-            <q>Genom låten Kokosboll kunde jag hålla mig borta från kriminalitet och droger.</q>
-            <br />- Rasmus Al-sudany - låtskrivare och arab.
-          </p>
-          <p>
-            Just nu tar vi inte in nya medlemmar, men ansök gärna om en plats genom vår mail:
-            <a href="mailto:alle.goransson@hotmail.com"> alle.goransson@hotmail.com</a>.
-          </p>
-        </div>
-        <div id="timeline">
-          <div className="timeline">
-            {timeline_content.map((content, idx) => {
-              return <TimeStamp stamp={content} key={idx} side={idx % 2 == 0 ? "left" : "right"} />;
-            })}
+    <>
+      <main id="start">
+        <div className="content">
+          <div id="header">
+            <h1>Morgonmysboisen</h1>
+            <Image src={"/images/grupp_bild.png"} width={2592} height={1434} alt="Bakgrund" />
+          </div>
+          <div className="link-list">
+            <Link href={"/lmm1"}>LMM1</Link>
+            <Link href={"/lmm2"}>LMM2</Link>
+            <Link href={"/lmm3"}>LMM3</Link>
+            <Link href={"/"}>Kokosboll</Link>
+            <Link href={"/"}>Art gallery</Link>
+            <Link href={"/"}>Behind the scenes</Link>
+          </div>
+          <div id="about">
+            <h2>Om Morgonmysboisen</h2>
+            <p>
+              Morgonmysboisen är en organisation vars syfte är att kunna erbjuda individer en plats
+              för roliga stunder och självklart mys.
+            </p>
+            <p className="review">
+              <q>Med Morgonmysboisen har jag kunnat integreras in i det svenska samhället.</q>
+              <br />- Benjamin Boros - gamer och kelgris.
+            </p>
+            <p>
+              Morgonmysboisen, tidigare kallad och även välkänd som Kvällsmysboisen, blev under 2022
+              ett officiellt kulturarv i Sverige efter organisationens framgångsrika arbete med
+              humor och glamor. Framgångarna såsom hit-triliogin &quot;Lir Med Morgonmysboisen&quot;
+              och den ökända låten &quot;Kokosboll&quot; satte organisationen på kartan och gjorde
+              Ljungby kommun till en plats för unga att utvecklas.
+            </p>
+            <p className="review">
+              <q>Genom låten Kokosboll kunde jag hålla mig borta från kriminalitet och droger.</q>
+              <br />- Rasmus Al-sudany - låtskrivare och arab.
+            </p>
+            <p>
+              Just nu tar vi inte in nya medlemmar, men ansök gärna om en plats genom vår mail:
+              <a href="mailto:alle.goransson@hotmail.com"> alle.goransson@hotmail.com</a>.
+            </p>
+          </div>
+          <div id="timeline">
+            <div className="timeline">
+              {timeline_content.map((content, idx) => {
+                return (
+                  <TimeStamp stamp={content} key={idx} side={idx % 2 == 0 ? "left" : "right"} />
+                );
+              })}
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+
+      <Footer />
+    </>
   );
 }
