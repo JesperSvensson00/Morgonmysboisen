59728618d05792f6cb272e95d6b37b0ec60c77c1
JesperSvensson00 <jesper.svensson00@hotmail.com>
2023-06-20 17:11:36 +0200

    La till videosidor för alla lmm videos

--- pages/lmm1.js
+++ pages/lmm1.js
@@ -1,11 +1,11 @@
-export default function Lmm3() {
+export default function Lmm1() {
   return (
-    <main id="lmm3" className="lmm">
+    <main id="lmm1" className="lmm">
       <video src="background.mp4" className="background" muted autoPlay loop />
       <div className="content">
         <div className="video-holder">
-          <h1>Lir med Morgonmysboisen III</h1>
-          <iframe src="https://www.youtube.com/embed/cP_h48Db69o" />
+          <h1>Lir med Morgonmysboisen I</h1>
+          <iframe src="https://www.youtube.com/embed/-yfVIjrjjAQ" />
         </div>
       </div>
     </main>
--- pages/lmm2.js
+++ pages/lmm2.js
@@ -1,11 +1,11 @@
-export default function Lmm3() {
+export default function Lmm2() {
   return (
-    <main id="lmm3" className="lmm">
+    <main id="lmm2" className="lmm">
       <video src="background.mp4" className="background" muted autoPlay loop />
       <div className="content">
         <div className="video-holder">
-          <h1>Lir med Morgonmysboisen III</h1>
-          <iframe src="https://www.youtube.com/embed/cP_h48Db69o" />
+          <h1>Lir med Morgonmysboisen II</h1>
+          <iframe src="https://www.youtube.com/embed/o-YO9vPv0bM" />
         </div>
       </div>
     </main>
